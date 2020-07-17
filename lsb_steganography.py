b9272d99f1e0ba02c6335ef0f576e588b5158f32
Philipp Mayer <philipp.mayer@phape.de>
2020-07-17 15:23:20 +0200

    changing pixel values is now possible

--- lsb_steganography.py
+++ lsb_steganography.py
@@ -14,40 +14,64 @@ class LsbStega():
 
     def text_to_bits(self, text, encoding='utf-8', errors='surrogatepass'):
         bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
-        return bits.zfill(8 * ((len(bits) + 7) // 8))
+        # return bits.zfill(8 * ((len(bits) + 7) // 8))
+        return self.fill_bits(bits, 8)
 
     def text_from_bits(self, bits, encoding='utf-8', errors='surrogatepass'):
         n = int(bits, 2)
         return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
 
+    def fill_bits(self, value, bitcount):
+        return value.zfill(bitcount * ((len(value) + (bitcount-1)) // bitcount))
+
     def encode_text(self, img_location, text):
         text_length_binary = bin(len(text))[2:]
-        text_length_bits = text_length_binary.zfill(16 * ((len(text_length_binary) + 15) // 16)) # 16 bits --> text length can be 2^16 = 65 536 chars
+
+        text_length_bits = self.fill_bits(text_length_binary, 16)
         print("text lenth bits:", text_length_bits)
+        text_as_bits = self.text_to_bits(text)
+        data_to_hide = text_length_bits + text_as_bits
+        print("data to hide:", data_to_hide)
 
         img = Image.open(img_location)
-        img_pixels = img.load()
+        src_img_pixels = img.load()
+        RED = 0
+        GREEN = 1
+        BLUE = 2
         img_x_size = img.size[0]
         img_y_size = img.size[1]
-        red_pixels = img.split()[0]
-        green_pixels = img.split()[1]
-        blue_pixels = img.split()[2]
-        # print(img_x_size)
-        # print(img_y_size)
 
         result_img = Image.new("RGB", (img_x_size, img_y_size))
+        result_img_pixels = result_img.load()
+
+        current_position = 0
         for y in range(img_y_size):
             for x in range(img_x_size):
-                print("x:", x, "y:", y, "rgb:", img_pixels[x, y], "bin:", bin(blue_pixels.getpixel((x, y)))[-1])
-                pass
-        pass
+                if current_position >= len(data_to_hide) or (bin(src_img_pixels[x, y][BLUE])[-1]) == data_to_hide[current_position]:
+                    # print("pixel is correct")
+                    result_img_pixels[x, y] = src_img_pixels[x, y]
+
+                else:
+                    print("pixel must be changed to",
+                          data_to_hide[current_position])
+                    pixel = src_img_pixels[x, y]
+                    blue_binary = self.fill_bits(bin(pixel[BLUE])[2:], 8)
+                    print("blue bits:", blue_binary)
+                    blue_binary = blue_binary[:-1] + \
+                        data_to_hide[current_position]
+                    print("blue bits new:", blue_binary)
+                    result_img_pixels[x, y] = (
+                        src_img_pixels[x, y][RED], src_img_pixels[x, y][GREEN], int(blue_binary, 2))
+                    print("src pixel:", src_img_pixels[x, y])
+                    print("result pixel:", result_img_pixels[x, y])
+
+                current_position += 1
+        return
 
 
 def main():
     stega = LsbStega()
-    # print(stega.to_binary('A'))
-    print("text-to-bits:", stega.text_to_bits('AB'))
-    stega.encode_text('./sample_picture.png', "hello my name is Philipp jasklfjweiojgonboanvadnvynjgojregoijaojvoyjvoijaagjioarjgoiajdivjdooiaiogjegaiondivnjkdjgnjgöarjojavonöafdnbklkdfjglfdkjgaoöerigjeikvfjmevfjefjöajfcöa")
+    stega.encode_text('./sample_picture.png', "A")
 
 
 if __name__ == "__main__":
